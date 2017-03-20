d3a31fb23a4064c9d4f7bfdabb52789f8e679540
razvanmiclau <razvan.miclau@gmail.com>
2017-03-20 14:57:07 +0000

    Imported Filestack API

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import { render } from 'react-dom';
 import Routes from './routes';
 import Home from './components/Home'
 
+// FileStack API
+filepicker.setKey("At2eWk3cXTt2E43Ypq9iXz");
+
 render(
   Routes,
   document.getElementById('root')

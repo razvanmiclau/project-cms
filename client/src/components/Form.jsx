56c2c4f6efd0d7f2c40d3352f7649d2aa7c3124e
razvanmiclau <razvan.miclau@gmail.com>
2017-03-28 14:13:02 +0100

    Added Form Component

---
+++ client/src/components/Form.jsx
@@ -0,0 +1,44 @@
+import React, { PureComponent } from 'react';
+import { Link } from 'react-router';
+import { Field, reduxForm } from 'redux-form/immutable';
+
+class Form extends PureComponent {
+  render() {
+    const { image, uploadImage } = this.props;
+    return (
+      <form onSubmit={this.props.submit}>
+        <div className="form-group">
+          <label htmlFor="project-title">Project Title</label>
+          <Field
+            name="title"
+            type="text"
+            className="form-control"
+            component="input"
+            placeholder="Project Title" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="project-url">Image URL</label>
+          <input type="file" onClick={uploadImage} />
+        </div>
+        <div className="form-group">
+          <img src={image} id="preview" className="img-responsive img-upload" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="project-description">Project Description</label>
+          <Field
+            name="desc"
+            type="text"
+            className="form-control"
+            rows="3"
+            component="textarea"
+            placeholder="Project Title" />
+        </div>
+
+        <button className="btn btn-primary" type="submit">Submit Project</button>
+        <Link className="btn" to='/projects'>Back</Link>
+      </form>
+    )
+  }
+}
+
+export default reduxForm({ form: 'project' })(Form);
